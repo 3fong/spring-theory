140057095e9de7cc0f6ed1d7c2ac44773d6bc3cf
“liulei” <871607680@qq.com>
2022-03-22 09:42:24 +0800

    update: 增加aop注解示例

--- src/main/java/com/ll/MyApplicationContext.java
+++ src/main/java/com/ll/MyApplicationContext.java
@@ -1,13 +1,17 @@
-package com.ll.context;
+package com.ll;
 
 import com.ll.bean.Car;
 import com.ll.config.ContextConf;
+import com.ll.service.CarService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MyContext {
+public class MyApplicationContext {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ContextConf.class);
         Car bean = context.getBean(Car.class);
         System.out.println(bean);
+        CarService carService = context.getBean(CarService.class);
+        String run = carService.run();
+        System.out.println(run);
     }
 }
--- src/main/java/com/ll/aop/LogAspect.java
+++ src/main/java/com/ll/aop/LogAspect.java
@@ -1,8 +1,7 @@
 package com.ll.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 /**
  * @author liulei
@@ -10,13 +9,35 @@ import org.aspectj.lang.annotation.Pointcut;
  * @create 2022/3/21 22:03
  */
 @Aspect
+@Component// 组件标识,用于将对象放入容器中
 public class LogAspect {
 
-    @Pointcut("execution(com.ll.service.*)")
+    // 切入点表达式要注意返回值,类,方法
+    @Pointcut("execution(* com.ll.service.*.*(..))")
     public void pointCut() {}
 
     @Before("pointCut()")
     public void before() {
         System.out.println("LogAspect before");
     }
+
+    @After("pointCut()")
+    public void after() {
+        System.out.println("LogAspect after");
+    }
+
+    @AfterReturning("pointCut()")
+    public void afterReturning(){
+        System.out.println("LogAspect afterReturning...");
+    }
+
+    @AfterThrowing("pointCut()")
+    public void afterThrowing(){
+        System.out.println("LogAspect afterThrowing...");
+    }
+    // around会覆盖其他切入注解
+//    @Around("pointCut()")
+//    public void around(){
+//        System.out.println("LogAspect around...");
+//    }
 }
